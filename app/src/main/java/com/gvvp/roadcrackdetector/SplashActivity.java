81b729f839c2fbbd2591981778fa9b9119cdf105
Lucifer7535 <pawarpranay749@gmail.com>
2023-04-30 10:48:12 +0530

    splash activity with top and bottom anim

---
+++ app/src/main/java/com/gvvp/roadcrackdetector/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.gvvp.roadcrackdetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView title, desc;
+
+    private static int SPLASH_SCREEN = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animaion);
+
+        image = findViewById(R.id.splashImage);
+        title = findViewById(R.id.splashTitle);
+        desc = findViewById(R.id.splashDesc);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        desc.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
