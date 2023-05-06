0a76983a6e41a0a904966a0abae440bdf28f4727
Lucifer7535 <pawarpranay749@gmail.com>
2023-05-06 20:27:44 +0530

    added maps with bottomsheet, detector activity and others

--- app/src/main/java/com/gvvp/roadcrackdetector/MainActivity.java
+++ app/src/main/java/com/gvvp/roadcrackdetector/MainActivity.java
@@ -1,14 +1,214 @@
 package com.gvvp.roadcrackdetector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.gvvp.roadcrackdetector.env.Logger;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RelativeLayout detectcrackbtn, showmapbtn, viewstatsbtn, editprofilebtn;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    public static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.15f;
+    public static final int PRIORITY_HIGH_ACCURACY = 100;
+
+    private FirebaseAuth mAuth;
+    public static double latitude;
+    public static double longitude;
+    private static final Logger LOGGER = new Logger();
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        detectcrackbtn = findViewById(R.id.roadcrackbutton);
+        showmapbtn = findViewById(R.id.mapcrackbutton);
+        viewstatsbtn = findViewById(R.id.crackstatsbutton);
+        editprofilebtn = findViewById(R.id.editprofilebutton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Menu menu = navigationView.getMenu();
+        MenuItem logout_btn = menu.findItem(R.id.nav_logout);
+
+        logout_btn.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                mAuth.signOut();
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isLoggedIn", false);
+                editor.apply();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                Toast.makeText(MainActivity.this, "Successfully logged out!", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        showmapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        detectcrackbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DetectorActivity.class);
+                startActivity(intent);
+            }
+        });
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+                MainActivity.this
+        );
+        this.getLoc();
+    }
+
+    public void getLoc() {
+        if(ActivityCompat.checkSelfPermission(MainActivity.this
+                , android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(MainActivity.this
+                , android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this
+                    , new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION
+                            , android.Manifest.permission.ACCESS_COARSE_LOCATION}
+                    , 100);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100 && grantResults.length > 0 && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+            getCurrentLocation();
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission denied.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getCurrentLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // Request location permission
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                return;
+            }
+
+            fusedLocationProviderClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null).addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+
+                    if (location != null) {
+                        LOGGER.i("*************" + location.getLatitude());
+                        LOGGER.i("*************" + location.getLongitude());
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                    } else {
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(100)
+                                .setFastestInterval(10)
+                                .setNumUpdates(3);
+
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                Location location1 = locationResult.getLastLocation();
+                                LOGGER.i("*************" + location1.getLatitude());
+                                LOGGER.i("*************" + location1.getLongitude());
+                                longitude = location1.getLongitude();
+                                latitude = location1.getLatitude();
+                            }
+                        };
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                                && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        } else {
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
---
+++ app/src/main/java/com/gvvp/roadcrackdetector/MapsActivity.java
@@ -0,0 +1,226 @@
+package com.gvvp.roadcrackdetector;
+
+import static android.graphics.BitmapFactory.decodeByteArray;
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.gvvp.roadcrackdetector.databinding.ActivityMapsBinding;
+import com.gvvp.roadcrackdetector.env.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private List<Marker> markerList;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location lastKnownLocation;
+    private static final int DEFAULT_ZOOM = 15;
+
+    private static final Logger LOGGER = new Logger();
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    View bottomSheet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setPeekHeight(200);
+        bottomSheetBehavior.setHideable(true);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        markerList = new ArrayList<>();
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        final String uid = currentUser.getUid();
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                lastKnownLocation = location;
+                LatLng latLng = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+            }
+        });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference usersRef = db.collection("Users");
+
+        usersRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                //mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(MapsActivity.this));
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot userDocument : task.getResult()) {
+                        DocumentReference userRef = usersRef.document(userDocument.getId());
+                        CollectionReference locationsRef = userRef.collection("locations");
+
+                        locationsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot locationDocument : task.getResult()) {
+                                        String label = locationDocument.getString("Label");
+                                        double latitude = locationDocument.getDouble("Latitude");
+                                        double longitude = locationDocument.getDouble("Longitude");
+
+                                        String addressLine = locationDocument.getString("Address Line");
+                                        double confidence = locationDocument.getDouble("Confidence");
+                                        String image = locationDocument.getString("Image");
+                                        String locality = locationDocument.getString("Locality");
+                                        String postalCode = locationDocument.getString("Postal Code");
+                                        String timeStamp = locationDocument.getString("TimeStamp");
+                                        String desc = addressLine + "\n" + timeStamp;
+                                        //Bitmap markerimage = decodeBase64ToBitmap(image);
+
+                                        LatLng location = new LatLng(latitude, longitude);
+
+                                        MarkerOptions markerOptions = new MarkerOptions()
+                                                .position(location)
+                                                .title(label)
+                                                .snippet(desc);
+
+                                        Marker newmarker = mMap.addMarker(markerOptions);
+                                        newmarker.setTag(image);
+                                        newmarker.setZIndex((float) confidence);
+                                        markerList.add(newmarker);
+                                    }
+
+                                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                                    for (Marker marker : markerList) {
+                                        builder.include(marker.getPosition());
+                                    }
+                                    LatLngBounds bounds = builder.build();
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+
+
+        //marker click listeners
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                updateBottomSheetContent(marker);
+                return true;
+            }
+        });
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+    }
+
+    //method to set view in bottom sheet
+    private void updateBottomSheetContent(Marker marker) {
+        String imageString = (String) marker.getTag();
+        ImageView imageView = bottomSheet.findViewById(R.id.marker_image);
+        byte[] imageBytes = Base64.decode(imageString, Base64.DEFAULT);
+        Bitmap crackimage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        if (imageString != null && !imageString.isEmpty()) {
+            Bitmap image = crackimage;
+            if (image != null) {
+                imageView.setImageBitmap(image);
+            }
+        }
+        String title = marker.getTitle();
+        TextView tvTitle = bottomSheet.findViewById(R.id.marker_title);
+        if (!title.equals("")) {
+            tvTitle.setText(title);
+        }
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = bottomSheet.findViewById(R.id.marker_desc);
+        String subsnippet = snippet.substring(0,snippet.indexOf("2023"));
+        if (!snippet.equals("")) {
+            tvSnippet.setText("Location: " +subsnippet);
+        }
+        String timestamp = marker.getSnippet();
+        TextView tvTimestamp = bottomSheet.findViewById(R.id.marker_ts);
+        String subtimestamp = timestamp.substring(timestamp.indexOf("2023"));
+        if (!subtimestamp.equals("")) {
+            tvTimestamp.setText(subtimestamp);
+        }
+        Float conf = marker.getZIndex();
+        String confidence = conf.toString();
+        TextView tvConf = bottomSheet.findViewById(R.id.marker_conf);
+        if(conf != null){
+            tvConf.setText(", Confidence:"+confidence);
+        }
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+}
